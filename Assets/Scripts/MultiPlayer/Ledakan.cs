afa423caf86ed921487e358e5422a7dd237f6b50
Mochamad Yusuf <mochyusuf100@gmail.com>
2018-08-06 23:18:38 +0700

    Sinkronisasi Player

---
+++ Assets/Scripts/MultiPlayer/Ledakan.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ledakan : Photon.PunBehaviour
+{
+    public string tagMusuh = "Musuh";
+    public int satuanSkor = 300;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (photonView.isMine)
+        {
+            if (other.gameObject.CompareTag(tagMusuh))
+            {
+                PhotonNetwork.player.AddScore(satuanSkor);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MultiPlayer/Tembakan.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tembakan : Photon.PunBehaviour
+{
+    public float kecepatanLaser;
+    private Vector3 syncPosShot = Vector3.zero;
+    public string tagMusuh = "Musuh";
+    public GameObject playerSaya;
+    public int satuanSkor = 100;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (photonView.isMine)
+        {
+            transform.position += transform.forward * Time.deltaTime * kecepatanLaser;
+        }
+    }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+        }
+        else
+        {
+            syncPosShot = (Vector3)stream.ReceiveNext();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (photonView.isMine)
+        {
+            if (other.gameObject.CompareTag(tagMusuh))
+            {
+                PhotonNetwork.player.AddScore(satuanSkor);
+            }
+        }
+    }
+}
